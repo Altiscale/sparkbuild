e7bd1de10a885dbebd7210880e759f144e9c1ba1
alee-altiscale <alee@altiscale.com>
2014-05-30 18:36:54 -0700

    no need to use clean option, use scrub option for mock clean up is sufficient

--- scripts/build.sh
+++ scripts/build.sh
@@ -164,12 +164,12 @@ mock -vvv --configdir=$curr_dir -r altiscale-spark-centos-6-x86_64.runtime --no-
 if [ $? -ne "0" ] ; then
   echo "fail - mock RPM build failed"
   cleanup_secrets
-  mock --configdir=$curr_dir -r altiscale-spark-centos-6-x86_64.runtime --clean
+  # mock --configdir=$curr_dir -r altiscale-spark-centos-6-x86_64.runtime --clean
   mock --configdir=$curr_dir -r altiscale-spark-centos-6-x86_64.runtime --scrub=all
   exit -99
 fi
 
-mock --configdir=$curr_dir -r altiscale-spark-centos-6-x86_64.runtime --clean
+# mock --configdir=$curr_dir -r altiscale-spark-centos-6-x86_64.runtime --clean
 mock --configdir=$curr_dir -r altiscale-spark-centos-6-x86_64.runtime --scrub=all
 
 # Delete all src.rpm in the RPMS folder since this is redundant and copied by the mock process
