27b7887e1db06f1f572c9254756edafba04e5a50
alee-altiscale <alee@altiscale.com>
2014-12-23 10:12:51 -0800

    [AE-922] Add SQLContext API test case

--- test_spark/test_spark_sql.sh
+++ test_spark/test_spark_sql.sh
@@ -9,7 +9,7 @@ curr_dir=`cd $curr_dir; pwd`
 kerberos_enable=false
 spark_home=$SPARK_HOME
 
-testcase_shell_file_01=$curr_dir/sparkshell_examples.txt
+testcase_shell_file_02=$curr_dir/sparksql_sqlcontext_examples.txt
 
 # Check RPM installation.
 
@@ -49,38 +49,25 @@ if [ ! -d $spark_home ] ; then
   exit -1
 fi
 
-if [ ! -f "$testcase_shell_file_01"  ] ; then
+if [ ! -f "$testcase_shell_file_02"  ] ; then
   echo "fail - missing testcase for spark, can't continue, exiting"
   exit -2
 fi
 
 pushd `pwd`
 cd $spark_home
-hdfs dfs -mkdir -p spark/test/graphx/followers
-hdfs dfs -put /opt/spark/graphx/data/followers.txt spark/test/graphx/followers/
-hdfs dfs -put /opt/spark/graphx/data/users.txt spark/test/graphx/followers/
-hdfs dfs -mkdir -p spark/test/decision_tree
-hdfs dfs -put /opt/spark/mllib/data/sample_tree_data.csv spark/test/decision_tree/
-hdfs dfs -mkdir -p spark/test/logistic_regression
-hdfs dfs -put /opt/spark/mllib/data/sample_libsvm_data.txt spark/test/logistic_regression/
-hdfs dfs -mkdir -p spark/test/kmean
-hdfs dfs -put /opt/spark/mllib/data/kmeans/kmeans_data.txt spark/test/kmean/
-hdfs dfs -mkdir -p spark/test/linear_regression
-hdfs dfs -put /opt/spark/mllib/data/ridge-data/lpsa.data spark/test/linear_regression/
-hdfs dfs -mkdir -p spark/test/svm
-hdfs dfs -put /opt/spark/mllib/data/sample_svm_data.txt spark/test/svm/
-hdfs dfs -mkdir -p spark/test/naive_bayes
-hdfs dfs -put /opt/spark/mllib/data/sample_naive_bayes_data.txt spark/test/naive_bayes/
+hdfs dfs -mkdir -p spark/test/resources
+hdfs dfs -copyFromLocal /opt/spark/examples/src/main/resources/* spark/test/resources/
 
-echo "ok - testing spark REPL shell with various algorithm"
+echo "ok - testing spark SQL shell with simple queries"
 
 LD_LIBRARY_PATH=/opt/hadoop/lib/native/ ./bin/spark-shell --master yarn --deploy-mode client --queue research --driver-memory 1024M << EOT
-`cat $testcase_shell_file_01`
+`cat $testcase_shell_file_02`
 EOT
 
 if [ $? -ne "0" ] ; then
-  echo "fail - testing shell for various algorithm failed!"
-  exit -3
+  echo "fail - testing shell for SparkSQL on HiveQL failed!!"
+  exit -4
 fi
 
 popd
