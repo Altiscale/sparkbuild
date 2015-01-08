eb85e7e1ab22b095e1cb53388f41d7f28f99617f
alee-altiscale <alee@altiscale.com>
2015-01-07 20:09:36 -0500

    [AE-874] Provide pyspark SparkSQL example for HiveContext

---
+++ test_spark/src/main/python/pyspark_hql.py
@@ -0,0 +1,17 @@
+import os
+
+from pyspark import SparkContext
+# sc is an existing SparkContext.
+from pyspark.sql import HiveContext
+
+if __name__ == "__main__":
+    sc = SparkContext(appName="PythonSQL")
+    sqlContext = HiveContext(sc)
+
+    sqlContext.sql("CREATE TABLE IF NOT EXISTS spark_python_hive_test_table (key INT, value STRING)")
+    sqlContext.sql("LOAD DATA LOCAL INPATH '/opt/spark/examples/src/main/resources/kv1.txt' INTO TABLE spark_python_hive_test_table")
+
+    # Queries can be expressed in HiveQL.
+    results = sqlContext.sql("FROM spark_python_hive_test_table SELECT key, value").collect()
+
+    sc.stop()
