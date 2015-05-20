f5639415230cfcc7fca76962dc411ee86f510458
alee-altiscale <alee@altiscale.com>
2015-05-20 15:25:04 -0400

    [AE-1112] Update SparkSQL test case

--- test_spark/test_spark_shell.sh
+++ test_spark/test_spark_shell.sh
@@ -43,7 +43,10 @@ hdfs dfs -put /opt/spark/mllib/data/sample_naive_bayes_data.txt spark/test/naive
 echo "ok - testing spark REPL shell with various algorithm"
 hadoop_snappy_jar=$(find $HADOOP_HOME/share/hadoop/common/lib/ -type f -name "snappy-java-*.jar")
 hadoop_lzo_jar=$(find $HADOOP_HOME/share/hadoop/common/lib/ -type f -name "hadoop-lzo-*.jar")
-spark_opts_extra="$spark_opts_extra --jars $hadoop_lzo_jar,$hadoop_snappy_jar"
+# The guava JAR here does not match the Spark's pom.xml which is looking for version 14.0.1
+# Hive comes with Guava 11.0.2
+guava_jar=$(find $HIVE_HOME/lib/ -type f -name "guava-*.jar")
+spark_opts_extra="$spark_opts_extra --jars $hadoop_lzo_jar,$hadoop_snappy_jar,$guava_jar"
 
 spark_event_log_dir=$(grep 'spark.eventLog.dir' /etc/spark/spark-defaults.conf | tr -s ' ' '\t' | cut -f2)
 
