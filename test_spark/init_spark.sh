203787075ec170470dc6cb472bb8d26064ac6ce8
alee-altiscale <alee@altiscale.com>
2015-07-21 10:22:22 -0400

    [AE-1273] Remove codes that are related to Kerberos, moving to Chef later, fix init_spark.sh

--- test_spark/init_spark.sh
+++ test_spark/init_spark.sh
@@ -29,7 +29,7 @@ fi
 
 # Check Spark RPM installation
 
-spark_installed=$(rpm -qa | grep alti-spark | grep $spark_version | grep -v test | wc -l)
+spark_installed=$(rpm -qa | grep alti-spark | grep $spark_version | grep -v test | grep -v example | wc -l)
 if [ "x${spark_installed}" = "x0" ] ; then
   >&2 echo "fail - spark for $spark_Version not detected or installed, can't continue, exiting"
   >&2 echo "fail - you should install spark via RPM, if you install them from binary distros, you will need to tweak these test case"
@@ -53,25 +53,5 @@ if [ ! -f $spark_conf ] ; then
 fi
 
 event_log_dir=$(grep 'spark.history.fs.logDirectory' $spark_conf | tr -s ' ' '\t' | cut -f2)
-spark_user_dir=/user/spark
 
-# TBD: These can be removed since we have added to Chef. Keep it here for
-# backward compatibility only.
-if [ "x${kerberos_enable}" = "xfalse" ] ; then
-  hdfs dfs -test -e "$event_log_dir"
-  if [ $? -ne "0" ] ; then
-    HADOOP_USER_NAME=hdfs hdfs dfs -mkdir -p $event_log_dir
-    HADOOP_USER_NAME=hdfs hdfs dfs -chmod 1777 $event_log_dir
-    HADOOP_USER_NAME=hdfs hdfs dfs -chown spark:spark $event_log_dir
-  fi
-  hdfs dfs -test -e "$spark_user_dir"
-  if [ $? -ne "0" ] ; then
-    HADOOP_USER_NAME=hdfs hdfs dfs -mkdir -p $spark_user_dir
-    HADOOP_USER_NAME=hdfs hdfs dfs -chown spark:users $spark_user_dir
-  fi
-fi
 hdfs dfs -mkdir $event_log_dir/$USER
-
-
-
-
