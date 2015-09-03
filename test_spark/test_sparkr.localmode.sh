926f28783dd51d8a26975f83bac4cf1ca8cbc0c8
alee-altiscale <alee@altiscale.com>
2015-09-03 06:54:48 -0400

    [AE-1369] Fix test case so SparkR only runs in local mode.

--- test_spark/test_sparkr.localmode.sh
+++ test_spark/test_sparkr.localmode.sh
@@ -7,9 +7,16 @@ curr_dir=`dirname $0`
 curr_dir=`cd $curr_dir; pwd`
 spark_home=$SPARK_HOME
 
+testcase_sparkr_file_01=$curr_dir/sparkr.test.txt
+
 if [ "x${spark_home}" = "x" ] ; then
   spark_home=/opt/spark
   echo "ok - applying default location /opt/spark"
+fi
+
+if [ ! -f "$testcase_sparkr_file_01"  ] ; then
+  echo "fail - missing testcase for sparkR, can't continue, exiting"
+  exit -2
 fi
 
 source $spark_home/test_spark/init_spark.sh
@@ -18,8 +25,8 @@ spark_version=$SPARK_VERSION
 
 pushd `pwd`
 cd $spark_home
-
-
+hdfs dfs -mkdir -p /user/$USER/spark/test/sparkr/
+hdfs dfs -put examples/src/main/resources/people.json /user/$USER/spark/test/sparkr/
 
 echo "ok - testing spark REPL shell with various algorithm"
 hadoop_snappy_jar=$(find $HADOOP_HOME/share/hadoop/common/lib/ -type f -name "snappy-java-*.jar")
@@ -31,10 +38,10 @@ spark_opts_extra="--jars $hadoop_lzo_jar,$hadoop_snappy_jar,$guava_jar"
 
 spark_event_log_dir=$(grep 'spark.eventLog.dir' /etc/spark/spark-defaults.conf | tr -s ' ' '\t' | cut -f2)
 
-# SPARK-6797, SparkR does not support YARN (yarn-client, yarn-cluster) mode in Spark 1.4.
-# Use localmode here. Make sure Workbench has sufficient memory if this is the case.
-
-./bin/sparkR --verbose --driver-memory 1024M --conf spark.eventLog.dir=${spark_event_log_dir}$USER/ $spark_opts_extra
+# ./bin/spark-shell --verbose --master yarn --deploy-mode client --queue research --driver-memory 1024M --conf spark.eventLog.dir=${spark_event_log_dir}$USER/ $spark_opts_extra << EOT
+# queue_name="--queue interactive"
+queue_name=""
+./bin/sparkR --verbose --driver-memory 1024M --conf spark.eventLog.dir=${spark_event_log_dir}$USER/ $spark_opts_extra $queue_name
 
 if [ $? -ne "0" ] ; then
   echo "fail - testing SparkR shell for various algorithm failed!"
