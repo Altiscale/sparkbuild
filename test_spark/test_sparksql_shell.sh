784a61972e9fd6889aa2767bf9d52f77d842c72b
alee-altiscale <alee@altiscale.com>
2015-11-05 17:40:02 -0800

    [AE-1486] Fix ClassNotFound on guava JAR in spark SQL and pyspark SQL test case

--- test_spark/test_sparksql_shell.sh
+++ test_spark/test_sparksql_shell.sh
@@ -55,7 +55,8 @@ do
 done
 hadoop_snappy_jar=$(find $HADOOP_HOME/share/hadoop/common/lib/ -type f -name "snappy-java-*.jar")
 hadoop_lzo_jar=$(find $HADOOP_HOME/share/hadoop/common/lib/ -type f -name "hadoop-lzo-*.jar")
-spark_opts_extra="$spark_opts_extra --jars $mysql_jars,$hadoop_lzo_jar,$hadoop_snappy_jar"
+guava_jar=$(find $HIVE_HOME/lib/ -type f -name "guava-*.jar")
+spark_opts_extra="$spark_opts_extra --jars $mysql_jars,$hadoop_lzo_jar,$hadoop_snappy_jar,$guava_jar"
 
 spark_files=$(find $hive_home/lib/ -type f -name "datanucleus*.jar" | tr -s '\n' ',')
 spark_files="$spark_files$mysql_jars,/etc/spark/hive-site.xml"
