4c000d993526e525ed199c55e58562f607aa9e4f
alee-altiscale <alee@altiscale.com>
2018-02-08 10:16:21 -0800

    Add DEBUG_MAVEN env flag

--- scripts/justbuild.sh
+++ scripts/justbuild.sh
@@ -123,7 +123,13 @@ fi
 #   mvn_release_flag="-Psnapshots"
 # fi
 
-mvn_cmd="mvn -U -X $hadoop_profile_str -Phive-thriftserver -Phadoop-provided -Phive-provided -Psparkr -Pyarn -Pkinesis-asl -DskipTests install"
+DEBUG_MAVEN=false
+if [ "x${DEBUG_MAVEN}" = "xtrue" ] ; then
+  mvn_cmd="mvn -U -X $hadoop_profile_str -Phive-thriftserver -Phadoop-provided -Phive-provided -Psparkr -Pyarn -Pkinesis-asl -DskipTests install"
+else
+  mvn_cmd="mvn -U $hadoop_profile_str -Phive-thriftserver -Phadoop-provided -Phive-provided -Psparkr -Pyarn -Pkinesis-asl -DskipTests install"
+fi
+
 echo "$mvn_cmd"
 $mvn_cmd
 
