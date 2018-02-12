ed848d6db88ca9a5de7e280519a9058b8a342108
alee-altiscale <alee@altiscale.com>
2018-02-12 09:35:07 -0800

    AE-2745: Fix --replaces bug and add post installation script

---
+++ scripts/rpm_scripts/alti-spark-after_install.sh
@@ -0,0 +1,25 @@
+#!bin/bash
+
+curr_dir=`dirname $0`
+curr_dir=`cd $curr_dir; pwd`
+
+if [ -f "$curr_dir/../setup_env.sh" ]; then
+  set -a
+  source "$curr_dir/../setup_env.sh"
+  set +a
+else
+  echo "FATAL: missing critical env script for post-installation script"
+  exit -1
+fi
+
+for f in `find /opt/alti-spark-${SPARK_VERSION}/assembly/target/scala-${SCALA_VERSION}/jars/ -name "*.jar"`
+do
+  ln -vsf $f /opt/alti-spark-${SPARK_VERSION}/lib/
+done
+
+ln -vsf /opt/alti-spark-${SPARK_VERSION}/examples/target/scala-${SCALA_VERSION}/jars/spark-examples_${SCALA_VERSION}-${SPARK_VERSION}.jar /opt/alti-spark-${SPARK_VERSION}/lib/
+ln -vsf /opt/alti-spark-${SPARK_VERSION}/common/network-yarn/target/scala-${SCALA_VERSION}/spark-${SPARK_VERSION}-yarn-shuffle.jar /opt/alti-spark-${SPARK_VERSION}/lib/
+ln -vsf /opt/alti-spark-${SPARK_VERSION}/sql/hive/target/spark-hive_${SCALA_VERSION}-${SPARK_VERSION}.jar /opt/alti-spark-${SPARK_VERSION}/lib/spark-hive_${SCALA_VERSION}.jar
+ln -vsf /opt/alti-spark-${SPARK_VERSION}/sql/hive-thriftserver/target/spark-hive-thriftserver_${SCALA_VERSION}-${SPARK_VERSION}.jar /opt/alti-spark-${SPARK_VERSION}/lib/spark-hive-thriftserver_${SCALA_VERSION}.jar
+
+exit 0
