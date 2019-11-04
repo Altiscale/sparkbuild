9a15e6b270673b110c656c5247db387ced7d75ce
Ganesh <i858464@c02w48e6htd6.dhcp.pal.sap.corp>
2019-11-04 15:19:14 -0800

    BDSGOLD-564: making the post-install and post-remove scripts as no-ops. Moving them selectively to spark cookbook recipe

--- scripts/rpm_scripts/alti-spark-yarn-shuffle-after_remove.sh
+++ scripts/rpm_scripts/alti-spark-yarn-shuffle-after_remove.sh
@@ -5,11 +5,4 @@ SCALA_VERSION=<%= scala_version %>
 
 echo "ok - post-uninstall script for <%= pkgname %> triggered"
 
-if [ -d /opt/alti-spark-${SPARK_VERSION}/lib ] ; then
-  rm -vrf /opt/alti-spark-${SPARK_VERSION}/lib/spark-${SPARK_VERSION}-yarn-shuffle.jar
-  echo "Removing /opt/alti-spark-${SPARK_VERSION}/lib in yarn-shuffle post-remove as it spark-${SPARK_VERSION}-yarn-shuffle.jar is removed"
-  rm -vrf /opt/alti-spark-${SPARK_VERSION}/lib
-fi
-rm -vrf /opt/alti-spark-${SPARK_VERSION}/common/network-yarn
-
 # Do NOT put exit here, this will trigger an exit all the way to the parent script hadoop_ecosystem_component_build.rb and justinstall.sh
